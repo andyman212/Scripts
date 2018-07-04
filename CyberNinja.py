51e0b2ab1110d620e50ba9bdd2d475819e45dd33
Andrew.Melsom <andrew.melsom@gmail.com>
2018-07-04 17:31:27 +0100

    Added range scan

---
+++ CyberNinja.py
@@ -0,0 +1,39 @@
+os = []
+osversion = []
+portstotest = []
+remoteserver = []
+remoteserverport = []
+persistent = []
+
+
+def getosinfo():
+
+
+def testconectivity():
+
+
+def reverseconnection():
+
+
+def setpersistent():
+
+
+def getshell():
+
+
+def enumos():
+
+
+def migrateprocess():
+
+
+def cleanup():
+
+
+def upload():
+
+
+def download():
+
+
+
